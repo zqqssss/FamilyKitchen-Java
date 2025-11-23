e6ace03de4ab00d7a7afe3d24bb66b2679bdfdda
zqs <2671527378@qq.com>
2025-11-23 10:51:29 +0800

    first commit

---
+++ src/main/java/com/zzz/familykitchen/FamilyKitchenApplication.java
@@ -0,0 +1,13 @@
+package com.zzz.familykitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyKitchenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyKitchenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzz/familykitchen/FamilyKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzz.familykitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyKitchenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
